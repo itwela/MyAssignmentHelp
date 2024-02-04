2078e8561e30ed5c0731b8852191a2de32354bbc
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2024-02-04 07:20:39 -0500

    animation updates

--- src/App.tsx
+++ src/App.tsx
@@ -85,7 +85,7 @@ function App() {
           <h1 id='title-1'>Get Assignment Help...</h1>
           <h1 id='title-2'>From World's No.1...</h1>
           <h1 id='title-3'>Assignment Help Company!</h1>
-          <img src={logo} alt="" id="logo-1" />
+          <img src={logo} alt="" id="logo-1" className="w-[100px]" />
         </div>
         <div id="welcome-slide" className="h-[100vh] overflow-hidden opacity-0 text-[2em] md:text-[3.7em] p-10 bg-[#ee8038] justify-center items-center text-white  absolute top-0 left-0 z-[999] w-full flex flex-col gap-[5em] md:gap-[4em]">
           <h1 id='welcome-title'>Welcome.</h1>
