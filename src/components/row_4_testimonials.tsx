7d7a69c2b218ebfab10a96922c9a8940fe4a35d5
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2024-02-04 16:08:52 -0500

    hover animation to testimonials

--- src/components/row_4_testimonials.tsx
+++ src/components/row_4_testimonials.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
 import rowfourdata from './row_4_data';
 
 
 export const Testimonials = () => {
+
+  const handleMouseEnter = (event: MouseEvent<HTMLDivElement>) => {
+    const darkDiv = event.currentTarget;
+    const cardTitle = darkDiv.querySelector('.card-title');
+    if (cardTitle) {
+      cardTitle.classList.add('orange-title');
+    }
+  };
+
+  const handleMouseLeave = (event: MouseEvent<HTMLDivElement>) => {
+    const darkDiv = event.currentTarget;
+    const cardTitle = darkDiv.querySelector('.card-title');
+    if (cardTitle) {
+      cardTitle.classList.remove('orange-title');
+    }
+  };
+
+
   return (
     <>
         <div id="the-form"></div>
@@ -19,9 +37,11 @@ export const Testimonials = () => {
             <div
               key={index}
               className="dark-div justify-evenly flex flex-col gap-4 rounded-[1.5em] min-w-[60%] md:min-w-[40%] h-[24em] md:h-[26em] p-7"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}   
             >
               <div className='flex flex-col gap-2'>
-                <h2 className="name font-black md:text-[1.5em]">{item.title}</h2>
+                <h2 className="card-title name font-black md:text-[1.5em]">{item.title}</h2>
                 <div className="star">{item.star}</div>
               </div>
               <p className='text-[0.8em] md:text-[1em] overflow-scroll items-start place-content-center text-[0.7em] md:text-[1em] h-[35%]'>{item.quote} </p>
